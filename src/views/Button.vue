0fd8207c21cf70db39ee155d1e8caa3bd491b184
henok <henok@kaexpertise.com>
2022-01-23 14:23:37 +0300

    first commit

---
+++ src/views/Button.vue
@@ -0,0 +1,64 @@
+<template>
+    <div 
+      class="button"
+      :class="type"
+      :disabled="disabled"
+      >
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'customButton',
+  props: {
+    type: {
+        type: String,
+        default: 'default'
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+  },
+  data(){
+      return{
+
+      }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.button {
+  width: fit-content;
+  text-align: center;
+  max-width: 400px;
+  padding: 1.2rem;
+  background: transparent;
+  --notchSize: 15px;
+  clip-path: 
+    polygon(
+      0% var(--notchSize), 
+      var(--notchSize) 0%, 
+      100% 0%, 
+      100% var(--notchSize), 
+      100% calc(100% - var(--notchSize)), 
+      calc(100% - var(--notchSize)) 100%, 
+      var(--notchSize) 100%, 
+      0 100% 
+    );
+  font-size: 1.5em;
+  cursor: pointer;
+  &.primary {
+    border: 2px solid #000;
+    background-color: #fff;
+    color: #000;
+  }
+  &.secondary {
+    background-color: #000;
+    color: #fff;
+  }
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Blank</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <div id="container">
+          <custom-button>Button</custom-button><br>
+          <custom-button type="primary">Button</custom-button><br>
+          <custom-button type="secondary">Button</custom-button>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import customButton from './Button.vue'
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    customButton
+  }
+});
+</script>
+
+<style scoped>
+#container {
+  margin-top: 2em;
+  margin-left: 2em;
+}
+</style>
